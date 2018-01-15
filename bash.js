b101c68d809b52ff31292caa01f0f2faeaec463f
Mimi Zhao <mimi.zhao@berkeley.edu>
2018-01-15 13:09:52 -0500

    Finished command arguments. To Do: NPM Modules, Handle Asynchronicity, Piping Data

--- bash.js
+++ bash.js
@@ -4,7 +4,7 @@ process.stdout.write('prompt > ');
 
 var pwddate = process.stdin.on('data', function (data) {
   var cmd = data.toString().trim();
-
+  process.stdout.write("\n");
   command.pwd(cmd);
 
   command.ls(cmd);
@@ -13,12 +13,26 @@ var pwddate = process.stdin.on('data', function (data) {
 
   command.env(cmd);
 
+  command.cat(cmd);
+
+  command.head(cmd);
+
+  command.tail(cmd);
+
+  command.wc(cmd);
+
+  command.sort(cmd);
+
+  command.uniq(cmd);
+
   if(cmd === 'date'){
     var time = new Date();
     var formatted = time.toLocaleString();
     process.stdout.write(formatted);
   }
 
-  process.stdout.write('\nprompt > ');
+
+process.stdout.write('prompt > ');
 });
+
  // console.log(process.env.PATH)
--- command.js
+++ command.js
@@ -40,10 +40,93 @@ module.exports = {
     if(cmd.substr(0, 4) === 'echo'){
       process.stdout.write(cmd.substr(5));
     }
+  },
+
+  cat: function(cmd){
+    if(cmd.substr(0, 3) === 'cat'){
+      var fileName = "./" + cmd.substr(4);
+
+      fs.readFile(fileName, 'utf8', function (err, data){
+        if(err) throw err;
+        process.stdout.write(data);
+      });
+    }
+  },
+
+  head: function(cmd){
+    if(cmd.substr(0, 4) === "head"){
+      var fileName = "./" + cmd.substr(5);
+      fs.readFile(fileName, 'utf8', function (err, data){
+        if(err) throw err;
+        data = data.split("\n");
+        data.length = 5;
+        data = data.join("\n");
+        process.stdout.write(data);
+      });
+    }
+  },
+
+  tail: function(cmd){
+    if(cmd.substr(0, 4) === "tail"){
+      var fileName = "./" + cmd.substr(5);
+      fs.readFile(fileName, 'utf8', function (err, data){
+        if(err) throw err;
+        data = data.split("\n");
+        data = data.slice(data.length - 5);
+        data = data.join("\n");
+        process.stdout.write(data);
+      });
+
+    }
+  },
+
+  wc: function(cmd){
+    if(cmd.substr(0, 2) === "wc"){
+      var fileName = "./" + cmd.substr(3);
+      fs.readFile(fileName, 'utf8', function (err, data){
+        if(err) throw err;
+        data = data.split("\n");
+        var length = data.length.toString();
+        process.stdout.write(length);
+      });
+    }
+  },
+
+  sort: function(cmd){
+    if(cmd.substr(0, 4) === 'sort'){
+      var fileName = "./" + cmd.substr(5);
+      fs.readFile(fileName, 'utf8', function (err, data){
+        if(err) throw err;
+        data = data.split("\n");
+        data = data.sort().join("\n");
+        process.stdout.write(data);
+      });
+    }
+  },
+
+  uniq: function(cmd){
+    if(cmd.substr(0, 4) === 'uniq'){
+      var fileName = "./" + cmd.substr(5);
+      fs.readFile(fileName, 'utf8', function (err, data){
+        if(err) throw err;
+        data = data.split("\n");
+        var nData = [];
+        for(var i = 0; i < data.length - 1; i++){
+          if(data[i] != data[i+1]){
+            nData.push(data[i]);
+          }
+        }
+        nData = nData.join("\n");
+
+
+        process.stdout.write(nData);
+      });
+    }
   }
 
 
-
 }
+
+
 
 
